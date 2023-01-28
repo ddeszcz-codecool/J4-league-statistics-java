4ad8db73ae955eebc9c7c80762c0a570a6d9ff3d
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-28 15:12:09 +0100

    x. refactor with relative path

--- src/main/java/com/codecool/leaguestatistics/Program.java
+++ src/main/java/com/codecool/leaguestatistics/Program.java
@@ -1,9 +1,16 @@
 package com.codecool.leaguestatistics;
 
 import com.codecool.leaguestatistics.controller.Season;
+import com.codecool.leaguestatistics.factory.NamesGenerator;
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class Program {
@@ -11,11 +18,5 @@ public class Program {
     public static void main( String[] args ) throws FileNotFoundException {
         Season season = new Season();
         season.run();
-
-
-
-
-
-
     }
 }
--- src/main/java/com/codecool/leaguestatistics/factory/NamesGenerator.java
+++ src/main/java/com/codecool/leaguestatistics/factory/NamesGenerator.java
@@ -1,11 +1,18 @@
 package com.codecool.leaguestatistics.factory;
 
+import com.codecool.leaguestatistics.Utils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * Provides random names for Players and Teams
@@ -20,31 +27,21 @@ public class NamesGenerator {
         return getRandomStringFromFile("CityNames.txt") + " " + getRandomStringFromFile("TeamNames.txt");
     }
 
-//    private static String getRandomStringFromFile(String fileName) {
-//        String str = "";
-//        ClassLoader classLoader = NamesGenerator.class.getClassLoader();
-//        try (Stream<String> stream = Files.lines(Paths.get(classLoader.getResource(fileName).getFile()), StandardCharsets.UTF_8)) {
-//            int lineCount = (int) stream.count();
-//            int randomNumber = Utils.getRandomValue(1, lineCount);
-//            str = Files.readAllLines(Paths.get(classLoader.getResource(fileName).getFile())).get(randomNumber);
-//        } catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        return str;
-//    }
+
 
 
     private static String getRandomStringFromFile(String fileName) {
         String str = "";
         List<String> fileLines = new ArrayList<>();
-
+        String filePath = fileName;
         try{
-//            URL url = NamesGenerator.class.getResource(fileName); // todo does not work
-            File myObj = new File("C:\\Users\\rgv468\\JavaGitProjects\\1. JAVA\\W4\\2.LS\\src\\main\\resources\\"+fileName);
+            URL url = NamesGenerator.class.getClassLoader().getResource(filePath);
+            File myObj = new File(url.getPath());
             Scanner myReader = new Scanner(myObj);
             while(myReader.hasNextLine()){
                 String data = myReader.nextLine();
                 fileLines.add(data);
+
             }
             str = fileLines.get(new Random().nextInt(fileLines.size()));
 
